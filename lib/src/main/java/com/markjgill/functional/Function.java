df38e44a32dd19e257b04231b3d15b002041062c
Mark Gill <markgillnz@gmail.com>
2023-06-01 21:29:45 +1200

    Remove functions tryOr() tryOrThrow()

--- lib/src/main/java/com/markjgill/functional/Function.java
+++ lib/src/main/java/com/markjgill/functional/Function.java
@@ -18,28 +18,4 @@ public class Function
   {
     return Function.<A, B, C>flip().apply(fn);
   }
-
-  public static <A, B> Function3<Function1<A, B>, B, Consumer<Throwable>, Function1<A, B>> tryOr()
-  {
-    return (fn, defaultVal, onFailure) -> val -> Try.of(() -> fn.apply(val))
-        .onFailure(onFailure)
-        .getOrElse(defaultVal);
-  }
-
-  public static <A, B> Function1<A, B> tryOr(Function1<A, B> fn, B defaultVal, Consumer<Throwable> onFailure)
-  {
-    return Function.<A, B>tryOr().apply(fn, defaultVal, onFailure);
-  }
-
-  public static <A, B> Function2<Function1<A, B>, Consumer<Throwable>, Function1<A, B>> tryOrThrow()
-  {
-    return (fn, onFailure) -> val -> Try.of(() -> fn.apply(val))
-        .onFailure(onFailure)
-        .get();
-  }
-
-  public static <A, B> Function1<A, B> tryOrThrow(Function1<A, B> fn, Consumer<Throwable> onFailure)
-  {
-    return Function.<A, B> tryOrThrow().apply(fn, onFailure);
-  }
 }
